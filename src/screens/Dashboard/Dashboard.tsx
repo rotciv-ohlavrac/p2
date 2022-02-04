c47fd1742a871b7b7e99283b1211d86b8c99e6eb
Victor Rodrigues <victorrodrigues@letz.app>
2022-02-04 17:30:04 -0300

    feat: Add UserInfo to Header

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Container, Title } from "./styles";
+
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/68085186?v=4",
+              }}
+            />
+            <User>
+              <UserGretting>Ola,</UserGretting>
+              <UserName>Victor</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,17 +1,60 @@
 import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  font-weight: bold;
-  color: ${({ theme }) => theme.colors.title};
+const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
-export { Container, Title };
+const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`;
+
+const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Photo = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+
+  border-radius: 10px;
+`;
+
+const User = styled.View`
+  margin-left: 17px;
+`;
+
+const UserGretting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(18)}px;
+`;
+
+export {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGretting,
+  UserName,
+  UserWrapper,
+};
