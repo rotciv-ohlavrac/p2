ee47eae58493c6a75d27cc2ce022498d3488c9f0
Victor Rodrigues <victorrodrigues@letz.app>
2022-02-12 15:26:44 -0300

    feat: Add types to TransactionCard

--- src/components/TransactionCard/styles.ts
+++ src/components/TransactionCard/styles.ts
@@ -2,10 +2,15 @@ import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 
+interface TransactionType {
+  type: "positive" | "negative";
+}
+
 const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
   padding: 17px 24px;
+  margin-bottom: 16px;
 `;
 
 const Title = styled.Text`
@@ -13,10 +18,12 @@ const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
-const Amount = styled.Text`
+const Amount = styled.Text<TransactionType>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(20)}px;
   margin-top: 2px;
+  color: ${({ theme, type }) =>
+    type === "positive" ? theme.colors.success : theme.colors.attention};
 `;
 
 const Footer = styled.View`
